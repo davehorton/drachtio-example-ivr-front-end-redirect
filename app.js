14c3a7800e53ba6444abbd111d3f666c06a76703
Dave Horton <daveh@beachdognet.com>
2017-04-13 11:01:44 -0400

    initial functional version

--- app.js
+++ app.js
@@ -1,67 +1,57 @@
 const drachtio = require('drachtio') ;
 const app = drachtio() ;
-const Mrf = require('drachtio-fsmrf') ;
-const mrf = new Mrf(app) ;
 const config = require('./config');
 const Logger = require('./lib/logger'); 
 const logger = new Logger( config.logging ) ;
+const Srf = require('drachtio-srf'); 
+const srf = new Srf(app);
+const Mrf = require('drachtio-fsmrf') ;
+const mrf = new Mrf(config.drachtio) ;
 const mwConnect = require('./lib/connect_to_ms')( mrf, logger) ;
 const mwAllocateEndpoint = require('./lib/allocate_endpoint')( logger) ;
+const routeCall = require('./lib/route_call');
 
-app.connect( config.drachtio ) 
-.on('connect', (err, hostport) => {
-  logger.info(`connected to drachtio listening on ${hostport}`) ;
-})
-.on('error', (err) => {
-  logger.error(`Error connecting to drachtio at ${config.drachtio}`, err) ;
-}) ;
+srf.connect( config.drachtio ) 
+.on('connect', (err, hostport) => { logger.info(`connected to drachtio listening on ${hostport}`) ;})
+.on('error', (err) => { logger.error(`Error connecting to drachtio at ${config.drachtio}`, err) ; }) ;
 
+srf.locals.logger = logger ;
 
-app.use('invite', (req, res, next) => {
+srf.use('invite', (req, res, next) => {
+  logger.debug(`${req.get('Call-Id')}: received call from ${req.callingNumber}`);
   req.on('cancel', () => { 
     logger.info(`call canceled by caller`);
     req.canceled = true ;
-    cleanUp(req.ms, req.ep); 
+    req.app.locals.cleanUp(); 
   }) ;
   next() ;
 }) ;
-app.use( 'invite', mwConnect ) ;
-app.use( 'invite', mwAllocateEndpoint) ;
+srf.use( 'invite', mwConnect ) ;
+srf.use( 'invite', mwAllocateEndpoint) ;
 
-app.invite( (req, res) => {
-  let ms = req.ms ;
-  let ep = req.ep ;
+srf.invite( (req, res) => {
+  let ms = req.app.locals.ms ;
+  let ep = req.app.locals.ep ;
+  req.app.locals.cleanUp = cleanUpMaker( ms, ep );
 
+  // might have got a cancel while we were allocating endpoint
   if( req.canceled === true ) {
     return ;
   }
   
-  res.send(183, 'Session Progress', {
-    body: ep.local.sdp
-  }) ;
-
-  ep.playCollect({
-    file: 'ivr/8000/ivr-please_reenter_your_pin.wav',
-    min: 1,
-    max: 8,
-    tries: 2,
-    timeout: 5000,
-    digitTimeout: 2000,
-    terminators: '#'
-  }, (err, results) => {
-    if( err ) { 
-        logger.error(`Error allocating endpoint`, err) ;
-        ms.disconnect() ;
-        return res.send(503, 'Media resource failure') ;    
-    }
-
-    logger.debug(`playCollect finished`, results) ;
-
-  });
-
+  routeCall( req, res, srf, ep) ;
 }) ;
 
-function cleanUp( ms, ep ) {
-  if( !!ep ) { ep.destroy() ; }
-  if( !!ms ) { ms.disconnect(); }
+// closure to allow it to be called more than once but only do the job once
+function cleanUpMaker( ms, ep ) {
+
+  let idx = 0  ;
+
+  return function() {
+    logger.debug(`cleaning up media ${idx}`);
+    if( idx++ < 1 ) {
+      if( !!ep ) { ep.destroy() ; }
+      if( !!ms ) { ms.disconnect(); }          
+    }
+  } ;
 }
